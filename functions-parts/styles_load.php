83182d5927c59d297bd2b5a843bed6ce4cf2722b
Dimitrniy Nikolenko <dimitrnklnk@gmail.com>
2023-12-30 21:59:30 +0300

    header start + colors

--- functions-parts/styles_load.php
+++ functions-parts/styles_load.php
@@ -6,6 +6,7 @@ function load_styles(){
   	wp_enqueue_style('style_min', get_template_directory_uri().'/static/css/style.min.css');
   	wp_enqueue_style('slick', get_template_directory_uri().'/static/libs/slick/slick.min.css');
   	wp_enqueue_style('slick_theme', get_template_directory_uri().'/static/libs/slick/slick-theme.min.css');
+	wp_enqueue_style('slick_theme', get_template_directory_uri().'/static/libs/slick/slick-theme.min.css');
 
 }; 
 add_action('wp_enqueue_scripts', 'load_styles', 10);
--- header.php
+++ header.php
@@ -23,26 +23,11 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'cooklook' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'К содержимому', 'cooklook' ); ?></a>
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$cooklook_description = get_bloginfo( 'description', 'display' );
-			if ( $cooklook_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $cooklook_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
+			<?php the_custom_logo(); ?>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
@@ -50,7 +35,7 @@
 			<?php
 			wp_nav_menu(
 				array(
-					'theme_location' => 'menu-1',
+					'theme_location' => 'main_menu',
 					'menu_id'        => 'primary-menu',
 				)
 			);
