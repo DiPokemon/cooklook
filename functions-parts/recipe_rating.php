22660c297087bb7d8b357bbcbecdf3bf8ed842c8
Dimitrniy Nikolenko <dimitrnklnk@gmail.com>
2024-03-27 22:22:36 +0300

    like/dislike functional

--- functions-parts/recipe_rating.php
+++ functions-parts/recipe_rating.php
@@ -1,77 +1,99 @@
 <?php
-// Обновление количества лайков
-function update_recipe_likes() {
+// Проверка голоса пользователя
+// Проверка голоса пользователя
+function check_user_vote() {
     $recipe_id = $_POST['recipe_id'];
-    $recipe_likes = carbon_get_post_meta($recipe_id, 'recipe_likes');
-    $recipe_dislikes = carbon_get_post_meta($recipe_id, 'recipe_dislikes');
+    $user_id = get_current_user_id();
 
-    // Увеличиваем количество лайков
-    $recipe_likes++;
-    carbon_set_post_meta($recipe_id, 'recipe_likes', $recipe_likes);
+    // Получаем голос пользователя для данного рецепта
+    $user_vote = get_user_vote_meta($user_id, $recipe_id);
 
-    // Проверяем, голосовал ли пользователь за эту запись
-    $user_vote = get_user_vote($recipe_id);
+    // Определяем тип кнопки на основе голоса пользователя
+    $button_type = 'none';
+    if ($user_vote === 'like') {
+        $button_type = 'like';
+    } elseif ($user_vote === 'dislike') {
+        $button_type = 'dislike';
+    }
 
-    // Возвращаем новые значения лайков и дизлайков, а также состояние голоса пользователя
-    echo json_encode(array('likes' => $recipe_likes, 'dislikes' => $recipe_dislikes, 'user_vote' => $user_vote));
+   
+    // Возвращаем состояние голоса пользователя и тип кнопки
+    echo json_encode(array('user_vote' => $user_vote, 'button_type' => $button_type));
     wp_die();
 }
-add_action('wp_ajax_update_recipe_likes', 'update_recipe_likes');
 
-// Обновление количества дизлайков
-function update_recipe_dislikes() {
+
+// Функция для определения типа кнопки (лайк или дизлайк) на основе голоса пользователя
+function get_button_type($recipe_id, $user_vote) {
+    if ($user_vote === 'like') {
+        return 'like';
+    } elseif ($user_vote === 'dislike') {
+        return 'dislike';
+    } else {
+        // Если пользователь еще не проголосовал, возвращаем пустую строку
+        return '';
+    }
+}
+
+// Функция для получения голоса пользователя
+function get_user_vote_meta($user_id, $recipe_id) {
+    $user_votes = get_user_meta($user_id, 'user_votes', true);
+    return isset($user_votes[$recipe_id]) ? $user_votes[$recipe_id] : 'none';
+}
+
+// Обновление голоса пользователя
+function update_user_vote() {
     $recipe_id = $_POST['recipe_id'];
+    $vote_type = $_POST['vote_type'];
+    $user_id = get_current_user_id();
+
+    // Получаем текущее количество лайков и дизлайков
     $recipe_likes = carbon_get_post_meta($recipe_id, 'recipe_likes');
     $recipe_dislikes = carbon_get_post_meta($recipe_id, 'recipe_dislikes');
 
-    // Увеличиваем количество дизлайков
-    $recipe_dislikes++;
+    if ($vote_type === 'like') {
+        // Если пользователь ставит лайк
+        $recipe_likes++;
+        // Если пользователь уже ставил дизлайк, уменьшаем количество дизлайков
+        if (get_user_vote_meta($user_id, $recipe_id) === 'dislike') {
+            $recipe_dislikes--;
+        }
+    } elseif ($vote_type === 'remove_like') {
+        // Если пользователь удаляет лайк
+        $recipe_likes--;
+    } elseif ($vote_type === 'dislike') {
+        // Если пользователь ставит дизлайк
+        $recipe_dislikes++;
+        // Если пользователь уже ставил лайк, уменьшаем количество лайков
+        if (get_user_vote_meta($user_id, $recipe_id) === 'like') {
+            $recipe_likes--;
+        }
+    } elseif ($vote_type === 'remove_dislike') {
+        // Если пользователь удаляет дизлайк
+        $recipe_dislikes--;
+    }
+
+    // Обновляем количество лайков и дизлайков в полях Carbon Fields
+    carbon_set_post_meta($recipe_id, 'recipe_likes', $recipe_likes);
     carbon_set_post_meta($recipe_id, 'recipe_dislikes', $recipe_dislikes);
 
-    // Проверяем, голосовал ли пользователь за эту запись
-    $user_vote = get_user_vote($recipe_id);
-
-    // Возвращаем новые значения лайков и дизлайков, а также состояние голоса пользователя
-    echo json_encode(array('likes' => $recipe_likes, 'dislikes' => $recipe_dislikes, 'user_vote' => $user_vote));
-    wp_die();
-}
-add_action('wp_ajax_update_recipe_dislikes', 'update_recipe_dislikes');
-
-// Удаление голоса пользователя
-function remove_vote() {
-    $recipe_id = $_POST['recipe_id'];
-    $recipe_likes = carbon_get_post_meta($recipe_id, 'recipe_likes');
-    $recipe_dislikes = carbon_get_post_meta($recipe_id, 'recipe_dislikes');
-
-    // Убираем голос пользователя
-    delete_user_vote($recipe_id);
+    // Обновляем информацию о голосе пользователя в метаданных его профиля
+    update_user_vote_meta($user_id, $recipe_id, $vote_type);
 
     // Возвращаем новые значения лайков и дизлайков
     echo json_encode(array('likes' => $recipe_likes, 'dislikes' => $recipe_dislikes));
     wp_die();
 }
-add_action('wp_ajax_remove_vote', 'remove_vote');
 
-// Проверка голоса пользователя
-function check_user_vote() {
-    $recipe_id = $_POST['recipe_id'];
-    $user_vote = get_user_vote($recipe_id);
-
-    // Возвращаем состояние голоса пользователя
-    echo json_encode(array('user_vote' => $user_vote));
-    wp_die();
+// Функция для обновления голоса пользователя
+function update_user_vote_meta($user_id, $recipe_id, $vote_type) {
+    $user_votes = get_user_meta($user_id, 'user_votes', true) ?: array();
+    $user_votes[$recipe_id] = $vote_type;
+    update_user_meta($user_id, 'user_votes', $user_votes);
 }
+
 add_action('wp_ajax_check_user_vote', 'check_user_vote');
+add_action('wp_ajax_update_user_vote', 'update_user_vote');
 
-// Функция для получения голоса пользователя
-function get_user_vote($recipe_id) {
-    $user_id = get_current_user_id();
-    $user_vote = get_post_meta($recipe_id, 'user_vote_' . $user_id, true);
-    return $user_vote ? $user_vote : 'none';
-}
-
-// Функция для удаления голоса пользователя
-function delete_user_vote($recipe_id) {
-    $user_id = get_current_user_id();
-    delete_post_meta($recipe_id, 'user_vote_' . $user_id);
-}
+add_action('wp_ajax_nopriv_check_user_vote', 'check_user_vote');
+add_action('wp_ajax_nopriv_update_user_vote', 'update_user_vote');
--- static/js/recipe-rating.js
+++ static/js/recipe-rating.js
@@ -1,108 +1,90 @@
 jQuery(document).ready(function ($) {
     // Функция для проверки состояния голоса пользователя и присвоения класса кнопке
     function checkUserVote() {
-        var recipeID = $('.like_btn').attr('id').split('-')[1];
-        $.ajax({
-            type: 'POST',
-            url: rating.ajax_url,
-            data: {
-                action: 'check_user_vote',
-                recipe_id: recipeID
-            },
-            success: function(response) {
-                if (response.user_vote === 'like') {
-                    $('.like_btn').addClass('user_liked');
-                } else if (response.user_vote === 'dislike') {
-                    $('.dislike_btn').addClass('user_disliked');
+        $('.like_btn, .dislike_btn').removeClass('active'); // Убираем класс .active у всех кнопок перед началом проверки
+    
+        $('.like_btn, .dislike_btn').each(function () {
+            var button = $(this);
+            var recipeID = button.attr('id').split('-')[1];
+            $.ajax({
+                type: 'POST',
+                url: rating.ajax_url,
+                data: {
+                    action: 'check_user_vote',
+                    recipe_id: recipeID
+                },
+                success: function(response) {
+                    response = JSON.parse(response); // Преобразуем строку в объект
+    
+                    // Проверяем, есть ли класс .active у кнопки и соответствует ли он голосу пользователя
+                    var isActiveButton = button.hasClass('active');
+                    var isCorrectVote = (response.user_vote === 'like' && button.hasClass('like_btn')) ||
+                                        (response.user_vote === 'dislike' && button.hasClass('dislike_btn'));
+    
+                    if (isCorrectVote && !isActiveButton) {
+                        button.addClass('active'); // Добавляем класс .active только если кнопка соответствует голосу пользователя и не имеет этого класса
+                    }
                 }
-            }
+            });
         });
     }
+    
 
     // Вызываем функцию checkUserVote() при загрузке страницы
     checkUserVote();
 
     // Обработчик клика по кнопке лайка
     $('.like_btn').on('click', function () {
-        var recipeID = $(this).attr('id').split('-')[1];
-        
-        // Проверяем, есть ли у кнопки класс 'user_liked'
-        if ($(this).hasClass('user_liked')) {
-            // Если есть, отправляем запрос на удаление голоса
-            $.ajax({
-                type: 'POST',
-                url: rating.ajax_url,
-                data: {
-                    action: 'remove_vote',
-                    recipe_id: recipeID
-                },
-                success: function(response) {
-                    $('.like_count').text(response.likes);
-                    $('.dislike_count').text(response.dislikes);
-                    // Убираем класс 'user_liked' с кнопки
-                    $('.like_btn').removeClass('user_liked');
+        var button = $(this);
+        var recipeID = button.attr('id').split('-')[1];
+        var userVote = button.hasClass('active') ? 'remove_like' : 'like'; // Проверяем, стоит ли уже лайк от пользователя
+
+        $.ajax({
+            type: 'POST',
+            url: rating.ajax_url,
+            data: {
+                action: 'update_user_vote',
+                recipe_id: recipeID,
+                vote_type: userVote // Передаем тип голоса (лайк, дизлайк или удаление голоса)
+            },
+            success: function (response) {
+                // Убираем класс .active у всех кнопок
+                $('.like_btn, .dislike_btn').removeClass('active');
+                // Добавляем класс .active только к нажатой кнопке
+                if (userVote === 'like') {
+                    button.addClass('active');
                 }
-            });
-        } else {
-            // Если класса нет, отправляем запрос на постановку лайка
-            $.ajax({
-                type: 'POST',
-                url: rating.ajax_url,
-                data: {
-                    action: 'update_recipe_likes',
-                    recipe_id: recipeID
-                },
-                success: function(response) {
-                    $('.like_count').text(response.likes);
-                    $('.dislike_count').text(response.dislikes);
-                    // Добавляем класс 'user_liked' к кнопке
-                    $('.like_btn').addClass('user_liked');
-                    // Убираем класс 'user_disliked' с кнопки дизлайка
-                    $('.dislike_btn').removeClass('user_disliked');
-                }
-            });
-        }
+            }
+        });
     });
 
     // Обработчик клика по кнопке дизлайка
     $('.dislike_btn').on('click', function () {
-        var recipeID = $(this).attr('id').split('-')[1];
-        
-        // Проверяем, есть ли у кнопки класс 'user_disliked'
-        if ($(this).hasClass('user_disliked')) {
-            // Если есть, отправляем запрос на удаление голоса
-            $.ajax({
-                type: 'POST',
-                url: rating.ajax_url,
-                data: {
-                    action: 'remove_vote',
-                    recipe_id: recipeID
-                },
-                success: function(response) {
-                    $('.like_count').text(response.likes);
-                    $('.dislike_count').text(response.dislikes);
-                    // Убираем класс 'user_disliked' с кнопки
-                    $('.dislike_btn').removeClass('user_disliked');
+        var button = $(this);
+        var recipeID = button.attr('id').split('-')[1];
+        var userVote = button.hasClass('active') ? 'remove_dislike' : 'dislike'; // Проверяем, стоит ли уже дизлайк от пользователя
+
+        $.ajax({
+            type: 'POST',
+            url: rating.ajax_url,
+            data: {
+                action: 'update_user_vote',
+                recipe_id: recipeID,
+                vote_type: userVote // Передаем тип голоса (лайк, дизлайк или удаление голоса)
+            },
+            success: function(response) {
+                // Обновляем отображение количества лайков и дизлайков
+                // $('.like_count').text(response.likes);
+                // $('.dislike_count').text(response.dislikes);
+
+                // Убираем класс .active у всех кнопок
+                $('.like_btn, .dislike_btn').removeClass('active');
+                // Добавляем класс .active только к нажатой кнопке
+                if (userVote === 'dislike') {
+                    button.addClass('active');
                 }
-            });
-        } else {
-            // Если класса нет, отправляем запрос на постановку дизлайка
-            $.ajax({
-                type: 'POST',
-                url: rating.ajax_url,
-                data: {
-                    action: 'update_recipe_dislikes',
-                    recipe_id: recipeID
-                },
-                success: function(response) {
-                    $('.like_count').text(response.likes);
-                    $('.dislike_count').text(response.dislikes);
-                    // Добавляем класс 'user_disliked' к кнопке
-                    $('.dislike_btn').addClass('user_disliked');
-                    // Убираем класс 'user_liked' с кнопки лайка
-                    $('.like_btn').removeClass('user_liked');
-                }
-            });
-        }
+            }
+        });
     });
+
 });
