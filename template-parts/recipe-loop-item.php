44cf55d147a2cb581e7c3a2672e58f61bd8850e9
Dimitrniy Nikolenko <dimitrnklnk@gmail.com>
2024-08-27 00:29:07 +0300

    img wrapper link

--- template-parts/recipe-loop-item.php
+++ template-parts/recipe-loop-item.php
@@ -32,9 +32,13 @@ $total_time_convert = convert_time_to_string($total_time);
     <div class="recipe_loop-wrapper">
         <div class="img_block">
             <?php if (get_the_post_thumbnail_url(null, 'large')) : ?>
-                <img loading="lazy" src="<?= get_the_post_thumbnail_url(null, 'large') ?>" alt="<?= __('Рецепт', 'cooklook') ?> <?= the_title() ?> на <?= $portions ?> порций">
+                <a class="img_wrapper_link" href="<?= get_permalink() ?>">
+                    <img loading="lazy" src="<?= get_the_post_thumbnail_url(null, 'large') ?>" alt="<?= __('Рецепт', 'cooklook') ?> <?= the_title() ?> на <?= $portions ?> порций">
+                </a>   
             <?php else: ?>
-                <img loading="lazy" src="<?= get_template_directory_uri() . '/static/img/no_image.png' ?>" alt="<?= __('Рецепт', 'cooklook') ?> <?= the_title() ?> на <?= $portions ?> порций">
+                <a class="img_wrapper_link" href="<?= get_permalink() ?>">
+                    <img loading="lazy" src="<?= get_template_directory_uri() . '/static/img/no_image.png' ?>" alt="<?= __('Рецепт', 'cooklook') ?> <?= the_title() ?> на <?= $portions ?> порций">
+                </a>
             <?php endif; ?>
             <div class="recipe_meta">
                 <div class="top_meta">
